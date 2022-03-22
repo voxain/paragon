b5a2026a28c216c129733d701239dfd2f3b086f6
Rene <info@rmcprod.me>
2022-03-22 21:21:38 +0100

    fix navbar being overlayed by products on mobile devices

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -28,7 +28,7 @@
 </div>
 
 
-<div class="navbar md:relative bg-base-100 md:mb-4 md:shadow-xl md:rounded-box fixed bottom-0 left-0 md:bottom-auto">
+<div class="navbar z-50 md:relative bg-base-100 md:mb-4 md:shadow-xl md:rounded-box fixed bottom-0 left-0 md:bottom-auto">
   <div class="flex flex-1">
     <a href="/" class="btn btn-ghost normal-case text-xl">
         <!-- desktop --><span class="hidden md:block">
@@ -93,7 +93,7 @@
     </div>
     <!-- mobile -->
     <a href="/" class="md:hidden btn btn-ghost normal-case text-xl">
-        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48" style=" fill:currentColor;"><path d="M 20.5 6 C 12.509634 6 6 12.50964 6 20.5 C 6 28.49036 12.509634 35 20.5 35 C 23.956359 35 27.133709 33.779044 29.628906 31.75 L 39.439453 41.560547 A 1.50015 1.50015 0 1 0 41.560547 39.439453 L 31.75 29.628906 C 33.779044 27.133709 35 23.956357 35 20.5 C 35 12.50964 28.490366 6 20.5 6 z M 20.5 9 C 26.869047 9 32 14.130957 32 20.5 C 32 23.602612 30.776198 26.405717 28.791016 28.470703 A 1.50015 1.50015 0 0 0 28.470703 28.791016 C 26.405717 30.776199 23.602614 32 20.5 32 C 14.130953 32 9 26.869043 9 20.5 C 9 14.130957 14.130953 9 20.5 9 z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48" style=" fill:currentColor;"><path d="M 20.5 6 C 12.509634 6 6 12.50964 6 20.5 C 6 28.49036 12.509634 35 20.5 35 C 23.956359 35 27.133709 33.779044 29.628906 31.75 L 39.439453 41.560547 A 1.50015 1.50015 0 1 0 41.560547 39.439453 L 31.75 29.628906 C 33.779044 27.133709 35 23.956357 35 20.5 C 35 12.50964 28.490366 6 20.5 6 z M 20.5 9 C 26.869047 9 32 14.130957 32 20.5 C 32 23.602612 30.776198 26.405717 28.791016 28.470703 A 1.50015 1.50015 0 0 0 28.470703 28.791016 C 26.405717 30.776199 23.602614 32 20.5 32 C 14.130953 32 9 26.869043 9 20.5 C 9 14.130957 14.130953 9 20.5 9 z"></path></svg>
     </a>
   </div>
   <div class="md:flex-none">
@@ -145,3 +145,4 @@
     </div>
   </div>
 </div>
+
