f7d6439456523a556959a4ed6159a3eb94742eb4
Rene <info@rmcprod.me>
2022-04-19 00:22:14 +0200

    change category link

--- src/components/groupCards.svelte
+++ src/components/groupCards.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#each category as categ}
-<a href="/category/{categ[0]}"><h3 class="mb-1 text-4xl font-bold uppercase mt-10">{categ[0]}</h3></a>
+<a href="/category/{categ[0]}"><h3 class="mb-1 text-4xl font-bold uppercase mt-10 text-center">{categ[0]}</h3></a>
     <div class="flex flex-row flex-wrap justify-center w-full">
         <ListCards products={categ[1]} />
     </div>
